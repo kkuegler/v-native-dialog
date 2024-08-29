f5c2906e2d6a61229d3965d140fd43b37b1d4610
Konrad Kügler <swamblumat-git@yahoo.de>
2024-08-29 21:48:23 +0200

    feat: Export more functions/types
    These are indeed intended to be consumed.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as NativeDialog } from "./components/native-dialog.vue";
 export { default as DynamicDialogOutlet } from "./components/dynamic-dialog-outlet.vue";
-export { useDialog } from "./use-dialog";
+export { useDialog, useAllDynamicDialogs, injectionKey } from "./use-dialog";
+export type { ResultPayload } from "./use-dialog";
